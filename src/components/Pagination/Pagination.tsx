a44ceeb219ce8536da11284d549cd55aef3be0ae
Gareth Jones <garryfella@gmail.com>
2019-09-28 11:05:12 +1000

    [WIP] Migrating Pagination component over to typescript

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,4 +1,3 @@
-// @flow strict
 import React from 'react';
 import classNames from 'classnames/bind';
 import { Link } from 'gatsby';
@@ -6,20 +5,15 @@ import { PAGINATION } from '../../constants';
 import styles from './Pagination.module.scss';
 
 type Props = {
-  prevPagePath: string,
-  nextPagePath: string,
-  hasNextPage: boolean,
-  hasPrevPage: boolean
+  prevPagePath: string;
+  nextPagePath: string;
+  hasNextPage: boolean;
+  hasPrevPage: boolean;
 };
 
 const cx = classNames.bind(styles);
 
-const Pagination = ({
-  prevPagePath,
-  nextPagePath,
-  hasNextPage,
-  hasPrevPage
-}: Props) => {
+const Pagination = ({ prevPagePath, nextPagePath, hasNextPage, hasPrevPage }: Props) => {
   const prevClassName = cx({
     'pagination__prev-link': true,
     'pagination__prev-link--disable': !hasPrevPage
@@ -33,10 +27,14 @@ const Pagination = ({
   return (
     <div className={styles['pagination']}>
       <div className={styles['pagination__prev']}>
-        <Link rel="prev" to={hasPrevPage ? prevPagePath : '/'} className={prevClassName}>{PAGINATION.PREV_PAGE}</Link>
+        <Link rel="prev" to={hasPrevPage ? prevPagePath : '/'} className={prevClassName}>
+          {PAGINATION.PREV_PAGE}
+        </Link>
       </div>
       <div className={styles['pagination__next']}>
-        <Link rel="next" to={hasNextPage ? nextPagePath : '/'} className={nextClassName}>{PAGINATION.NEXT_PAGE}</Link>
+        <Link rel="next" to={hasNextPage ? nextPagePath : '/'} className={nextClassName}>
+          {PAGINATION.NEXT_PAGE}
+        </Link>
       </div>
     </div>
   );
